7cf62d911e81aa01e50ae11372f428148aeda864
Taehwa-kwon <65379567+Taehwa-kwon@users.noreply.github.com>
2020-05-26 14:29:22 +0900

    Update TestMain.java

--- DBTest02/src/TestMain.java
+++ DBTest02/src/TestMain.java
@@ -1,167 +1,84 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
-public class DBhandler {
-	private static String Driver = "oracle.jdbc.OracleDriver";
-	private static String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	private static String dbuid = "sys as sysdba";
-	private static String dbpwd = "1234";
-	
-	private Connection conn;
-	private Statement stmt;
-	private ResultSet rs; //oracle의 결과물에 첫번째 위치에 커서를 주고있다. 
-	
-	//생성자란 객체가 생성될때 반드시 실행되어야 할것들
-	public DBhandler() { //1.
-		open();				//1.
-	}
+public class TestMain { //4.
 
-	public void open() {		//2.
-		//Class.forName(Driver);  2.첫번째 써라운드 
-		try {
-			Class.forName(Driver);
-			//conn = DriverManager.getConnection(url,dbuid,dbpwd); 2.두번째 써라운드
-			conn = DriverManager.getConnection(url,dbuid,dbpwd);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void close() {		//3.
-		try {
-			if(rs != null ) rs.close();	
-			if(stmt != null ) stmt.close();
-			if(conn != null ) conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	//member 목록조회 MID, NAME, EMAIL, MLEVEL, RDATE
-	public List<Member> getMemberList() {   //8.메소드 작성
-		List<Member> list = new ArrayList<>();//11. ArrayList생성방법임. List 컬렉션에 select 한것을 담기위해서 
-		
-		//stmt = conn.createStatement();//9.surround 
-		try {
-			stmt = conn.createStatement();//10
-			String sql = " SELECT MID, NAME, EMAIL, ML.MLEVEL, ";	//10
-			sql += " TO_CHAR(RDATE,'YY-MM-DD') RDATE";
-			sql += " FROM MEMBER M, MEMBERLEVEL ML ";
-			sql += " WHERE M.POINT BETWEEN ML.LOPOINT AND ML.HIPOINT ";
+		public static void main(String[] args) {
+			DBhandler db = new DBhandler();//4. 목적은 생성자를 생성하는 것 . 생성자에 저렇게 db.open()을 넣어두면  객체를 안만들어도 가능함 .
 			
+			//여러명의 목록조회
 			
-			 ResultSet rs = stmt.executeQuery(sql); //10.이것의 executeQuery 리턴값이 ResultSet 이니깐  ResultSet으로 받아줘야함   //자동 commit이 된다. 
-			while(rs.next()) {
-				int mid = rs.getInt(1);//13
-				String name = rs.getString(2);
-				String email = rs.getString(3);
-				String mlevel =rs.getString(4);
-				String rdate =rs.getString(5);
-				
-				Member m = new Member( mid, name, email, mlevel, rdate); //12 생성자 생성 
-				list.add(m);//12
+			List<Member> list = db.getMemberList();//4.적어주고  			//7.create 메소드
+			
+			//List 의 경우 배열의 일종이지만 자료형의 개수가 가변인 상황에서 사용이 유리하다. 
+				//제네릭
+			
+			//ArrayList aList = new ArrayList();
+			//aList.add("hello");
+			//aList.add("java");
+			//예전의 자바에서 이렇게 타입설정 안하고 넣는것은 문제가 아니지만 꺼낼때는 아래처럼 해야해서 귀찮음. 
+			//String hello = (String) aList.get(0); // Object 를 String 으로 캐스팅한다. (String)
+			//String java = (String) aList.get(1);
+			
+			//이게 제네릭스의 장점임 
+			//ArrayList<String> aList = new ArrayList<String>();
+			//aList.add("hello");
+			//aList.add("java");
+			//String hello = aList.get(0);
+			//String java = aList.get(1);
+			
+			for (Member member : list) { //4
+				//System.out.println(member.toString());
+				System.out.println(member.listinfo()); //16.
 			}
 			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-		return list;
-	}
-
-	public Member getMember(int mid) { //18
-		Member m = null; //18 처음에 null값을 줘서 값이 있으면 
-		
-		try {//19
-			stmt = conn.createStatement();
-			String sql = "SELECT MID, NAME, EMAIL, PHONE, POINT, ML.MLEVEL, ";	  
-				sql += " TO_CHAR(RDATE,'YYYY-MM-DD DAY') RDATE " ;
-				sql += " FROM MEMBER M, MEMBERLEVEL ML ";
-				sql += " WHERE M.POINT BETWEEN ML.LOPOINT AND ML.HIPOINT AND M.MID = " + mid;
-				rs = stmt.executeQuery(sql); 
-				
-				if( rs.next()) {  //20-1 mid는 하나의 자료만 나타나니깐 while쓸 필요는없다. 근데 mid =4 면은 오류가 아니라 아무것도 검색결과가 나오지 않는다.
-									//그러니깐 if안에서 rs.next가 true이면 아래를 실행한다.
-					int mid2 = rs.getInt(1);
-					String name = rs.getString(2);
-					String email = rs.getString(3);
-					String phone = rs.getString(4);
-					int point = rs.getInt(5);
-					String mlevel = rs.getString(6);
-					String rdate = rs.getString(7);
-					
-					m= new Member (mid2,name,email,phone,point,mlevel, rdate);
-					
-				}
-				
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return m;
-	}
-	
-	
-	public int addMember(Member m) {
-		
-		int aftcnt = 0;
-		try {
-			stmt = conn.createStatement();
-			String sql = "INSERT INTO MEMBER (MID,NAME,EMAIL,PHONE) " ;
-			sql += "VALUES (( SELECT NVL(MAX(MID),0) +1 FROM MEMBER), ";
-			//sql += " values (15, ";
-			sql += " '" + m.getName() + "',";
-			sql += " '" + m.getEmail() + "',";
-			sql += " '" + m.getPhone() + "')";
 			
-			aftcnt = stmt.executeUpdate(sql);
-			System.out.println(aftcnt + "건이 ");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return aftcnt;
-	}
-	
-	public int delMember(int del_mid) {
-		int cnt = 0;
-		
-		try {
-			stmt = conn.createStatement();
-			String sql = " delete from member where mid=" + del_mid ;
-			cnt=stmt.executeUpdate(sql);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return cnt;
-	}
-
-	public int updateMember(Member um) {
-		int Cnt = 0;
-		try {
-			stmt = conn.createStatement();
-			String sql = "update MEMBER SET NAME = '"+um.getName()+"', email = '"+um.getEmail() +"', phone ='"+um.getPhone()+"', point = '"+um.getPoint()+"' where mid ="+um.getMid();
-			Cnt = stmt.executeUpdate(sql);
+			//ArrayList<String> aList = new ArrayList<String>();
+			//String자리에는 다양한 자료형이 올수 있다. 
 			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return Cnt;
+			//개인조회 18
+			System.out.println("======================");
+			int mid = 3;//18
+			Member m = db.getMember(mid); //18
+			if( m== null) System.out.println(mid + "번 자료가 없습니다."); //21
+			System.out.println(m.info()); //22
+			//db.close();//4.
+			
+			
+			//추가 
+//			Member m2 = new Member("은영4","yy@naver.com","010-5555-1111");
+//			db.addMember(m2);
+//			int cnt = db.addMember(m2);
+//			System.out.println(cnt +"건이 추가 되었습니다");
+//			
+//			List<Member> list2 = db.getMemberList();
+//			for (Member member : list2) { //4
+//				//System.out.println(member.toString());
+//				System.out.println(member.listinfo()); //16.
+//			}
+//			
+//			//삭제
+//			int del_mid =1;
+//			int cnt2 = 0;
+//			 cnt2 =db.delMember(del_mid);
+//			if(cnt2 ==0) 
+//				System.out.println("삭제 안됐습니다");
+//			else 
+//				System.out.println("삭제 완료 ");
+			
+			//업데이트  name, email, phone, point 
+			System.out.println("수정");
+			Member um = new Member(8,"형호","hy@naver.com","010-9655-6432",2000);
+			int cnt3 =0;
+			cnt3 = db.updateMember(um);
+			
+			if(cnt3==0)
+				System.out.println("수정 불가");
+			else 
+				System.out.println(mid+"번 자료가 " + cnt3 + "건 수정완료 ");
+			
+			db.close();
+			
 	}
 
 
-	
-	
-	
 }
